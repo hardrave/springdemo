210db7d9ecc568eff75333b229dc90a962557811
Michal <scientific.shelter@gmail.com>
2023-05-29 08:06:12 +0200

    redirection added

--- src/main/java/com/michal/demo/web/HomeController.java
+++ src/main/java/com/michal/demo/web/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.michal.demo.domain.User;
 
@@ -15,8 +16,16 @@ public class HomeController {
 		model.addAttribute("formData", new User());
 		return "index";
 	}
+	
 	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String processFormData(User user, RedirectAttributes attr) {
+		attr.addFlashAttribute("user", user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping(value="/display", method=RequestMethod.GET)
 	public String processFormData(User user) {
 		return "result";
 	}
+	
 }
